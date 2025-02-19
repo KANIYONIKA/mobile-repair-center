f0da11f2d9111b5d63b6253f7907dfe3bd64f019
KANIYO <ks.develox@gmail.com>
2025-02-19 11:01:34 +0900

    commit

--- src/app/_components/OpenShopPage.tsx
+++ src/app/_components/OpenShopPage.tsx
@@ -17,7 +17,7 @@ const OpenShopPage: React.FC<Props> = ({ shopInformation }) => {
                         {/* <img src="/img/open_new_window.svg" className="mr-2 h-auto w-5" /> */}
                         <img src="/img/open.svg" className="mr-2 h-auto w-8" />
                         <b>
-                            <div className="font-notoSansJP text-2xl text-primary-text1">{shopInformation.shortName}を利用する</div>
+                            <div className="font-notoSansJP text-2xl text-primary-text1">{shopInformation.shortName}を希望する（店舗ページが開きます）</div>
                         </b>
                     </div>
                 </a>
